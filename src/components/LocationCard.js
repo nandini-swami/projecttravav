a282185e8e0149683695bd90bd896f383578d5e0
nandini-swami <nandini.swami.18@gmail.com>
2024-12-02 17:54:47 -0500

    fixed the locationcards for better ui and reading in data

--- src/components/LocationCard.js
+++ src/components/LocationCard.js
@@ -1,21 +1,22 @@
 import React from 'react';
+import '../components/LocationCard.css';
 
 function LocationCard({ location }) {
   return (
     <div className="location-card">
       <h3 className="location-name">{location.name}</h3>
-      <p className="location-description">{location.description}</p>
+      <div className="location-rating">{location.rating}</div>
       <div className="location-tags">
         {location.tags.map((tag, index) => (
           <span key={index} className="tag">{tag}</span>
         ))}
       </div>
-      <div className="location-info">
+      <p className="location-description">{location.description}</p>
+      {/* <div className="location-info">
         <p className="location-address">{location.address}</p>
         <p className="location-contact">{location.contact}</p>
         <a href={location.website} target="_blank" rel="noopener noreferrer" className="location-website">Visit Website</a>
-      </div>
-      <div className="location-rating">{location.rating}</div>
+      </div> */}
     </div>
   );
 }
