48c3ff4c74dfd32a248507ccbe3909d59b238997
nandini-swami <nandini.swami.18@gmail.com>
2024-12-06 09:09:03 -0500

    implemented switch to local finds

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import HomePage from './HomePage/HomePage'; // Import HomePage
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'; // Import routing components
 import BookmarkPage from './BookmarkPage/BookmarkPage'; // Assuming you have a BookmarkPage component
 import MyTripsPage from './MyTripsPage/MyTripsPage'; // Assuming you have a MyTripsPage component
-import HomeLocalFindsPage from './HomeLocalFindsPage/HomeLocalFindsPage';
 import CategoriesPage from './CategoriesPage/CategoriesPage';
 
 function App() {
@@ -19,7 +18,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/bookmarkpage" element={<BookmarkPage />} />
         <Route path="/mytripspage" element={<MyTripsPage />} />
-        <Route path="/homelocalfinds" element={<HomeLocalFindsPage />} />
+        <Route path="/homelocalfinds" element={<HomePage />} />
         <Route path="/categories" element={<CategoriesPage />} />
       </Routes>
     </Router>
